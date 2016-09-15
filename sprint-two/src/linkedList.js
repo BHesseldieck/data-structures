b83bca3e2863dbef5d4f42f071bfbbb51d6f44be
BHesseldieck <b.hesseldieck@gmail.com>
2016-09-15 10:20:05 -0700

    Complete linkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,52 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    
+    if (list.head === null && list.tail === null) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+
+      var findLastNode = function(node) {
+      
+        if (node.next !== null) {
+          findLastNode(node.next);
+        } else {
+          return node;
+        } 
+      };
+
+      var lastNode = findLastNode(list.head);
+
+      lastNode.next = Node(value);
+
+      list.tail = Node(value);
+
+    }
   };
 
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {
+    var searchNodes = function(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next !== null) {
+        return searchNodes(node.next);
+      }
+
+      return false;
+    };
+
+    console.log(searchNodes(list.head));
+
+    var result = searchNodes(list.head);
+    return result;
+
   };
 
   return list;
@@ -27,3 +67,52 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// list = {
+//   head: {
+//     value
+//     next = {
+//       value
+//       next = {
+//         value
+//         next = null
+//       }
+//     }
+//   }
+//   tail: {
+//     value
+//     next = null
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
