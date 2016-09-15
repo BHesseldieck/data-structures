1848807a6ea72d8f6ddf5dc18588acf8f2c2dcc2
BHesseldieck <b.hesseldieck@gmail.com>
2016-09-14 20:06:12 -0700

    Complete the prototypal pattern.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newObj = Object.create(queueMethods);
+  return newObj;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  length: 0,
+  size: function () {
+    return this.length;
+  },
+  enqueue: function (val) {
+    this[this.length] = val;
+    this.length++;
+  },
+  dequeue: function () {
+    if (this.length > 0) {
+      this.length--;
+    }
+
+    var result = this[0];
+
+    for (var i = 0; i < this.length; i++) {
+      this[this.length - 1] = this[this.length];
+    }
+
+    delete this[this.length];
+
+    return result;
+  } 
+};
 
 
